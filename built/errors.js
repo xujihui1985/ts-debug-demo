43f5116e6e1189fc0ff6dd155db7327d72eb2018
Sean <xujihui1985@gmail.com>
2016-12-11 21:18:24 +0800

    update

--- built/errors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class MyError extends Error {
-    constructor(message) {
-        super(message);
-        this.description = 'this is a description';
-    }
-}
-exports.MyError = MyError;
-//# sourceMappingURL=errors.js.map
